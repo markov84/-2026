ef38e78479097f2484e24652251b7b3066dd2f41
markov84 <inj.markov84@gmail.com>
2026-05-20 16:15:27 +0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@
 import ReactDOM from "react-dom/client";
 import { Alert, Box, Button, Stack, Typography } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import App from "./App.jsx";
 import { AuthProvider } from "./providers/AuthProviderStable.jsx";
 import AppThemeProvider from "./providers/AppThemeProvider.jsx";
@@ -63,6 +64,7 @@ ReactDOM.createRoot(rootElement).render(
       <AppErrorBoundary>
         <AuthProvider>
           <App />
+          <Toaster position="top-right" />
         </AuthProvider>
       </AppErrorBoundary>
     </AppThemeProvider>
